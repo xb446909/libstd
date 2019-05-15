34a68c110695c9c596e0d80426487c38b3384fc6
Xiong <xiongblw0852@hanslaser.com>
2019-05-15 16:50:19 +0800

    临时提交

--- src/csv/csv.cpp
+++ src/csv/csv.cpp
@@ -3,27 +3,27 @@
 #include "CSVManager.h"
 #include <string.h>
 
-CSVManager g_csvInst;
+shared_ptr<CSVManager> g_csvInst = shared_ptr<CSVManager>(new CSVManager());
 
 int __stdcall OpenCSVFile(const char * szPath, int nFlag)
 {
-	return g_csvInst.Open(szPath, nFlag);
+	return g_csvInst->Open(szPath, nFlag);
 }
 
 std::vector<std::vector<std::string>> __stdcall GetAllData()
 {
-	return g_csvInst.GetData();
+	return g_csvInst->GetData();
 }
 
 int __stdcall GetStdStringData(int nRow, int nCol, std::string & str)
 {
-	return g_csvInst.GetData(nRow, nCol, str);
+	return g_csvInst->GetData(nRow, nCol, str);
 }
 
 int __stdcall GetDoubleData(int nRow, int nCol, double & dbVal)
 {
 	std::string str;
-	int nRet = g_csvInst.GetData(nRow, nCol, str);
+	int nRet = g_csvInst->GetData(nRow, nCol, str);
 	if (nRet == CSV_SUCCESS)
 	{
 		dbVal = atof(str.c_str());
@@ -34,7 +34,7 @@ int __stdcall GetDoubleData(int nRow, int nCol, double & dbVal)
 int __stdcall GetIntData(int nRow, int nCol, int & nVal)
 {
 	std::string str;
-	int nRet = g_csvInst.GetData(nRow, nCol, str);
+	int nRet = g_csvInst->GetData(nRow, nCol, str);
 	if (nRet == CSV_SUCCESS)
 	{
 		nVal = atoi(str.c_str());
@@ -45,7 +45,7 @@ int __stdcall GetIntData(int nRow, int nCol, int & nVal)
 int __stdcall GetStrData(int nRow, int nCol, char * szBuf)
 {
 	std::string str;
-	int nRet = g_csvInst.GetData(nRow, nCol, str);
+	int nRet = g_csvInst->GetData(nRow, nCol, str);
 	if (nRet == CSV_SUCCESS)
 	{
 		strcpy(szBuf, str.c_str());
@@ -55,6 +55,6 @@ int __stdcall GetStrData(int nRow, int nCol, char * szBuf)
 
 int __stdcall WriteAllData(std::vector<std::vector<std::string>> data)
 {
-	return g_csvInst.WriteData(data);
+	return g_csvInst->WriteData(data);
 }
 
